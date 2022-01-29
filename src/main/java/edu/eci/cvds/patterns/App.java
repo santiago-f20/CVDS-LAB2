319b6ff65fa6f4a242a513c33a9f75690a5396da
unknown <santiago.fetecua@labinfo.is.escuelaing.edu.co>
2022-01-29 09:35:33 -0500

    Compilar y ejecutar

--- src/main/java/edu/eci/cvds/patterns/App.java
+++ src/main/java/edu/eci/cvds/patterns/App.java
@@ -1,13 +1,14 @@
 package edu.eci.cvds.patterns;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+{	
+    public static void main(String[] args ){
+    	System.out.println("Hello " + args[0]);
     }
 }
